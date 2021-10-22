04083d5845725f5f73d24ff8ab79ff3e4894bc54
Ayush <18EGJCS024@GITJAIPUR.COM>
2021-10-22 05:58:05 +0530

    disabling buttons implemented

--- Components/Verify.js
+++ Components/Verify.js
@@ -31,6 +31,8 @@ export default function Verify({navigation}) {
   const [teleg, setteleg] = useState(0);
   const [ytu, setytu] = useState(0);
   const [unkid, setunkid] = useState('');
+  const [disableResend, setdisableResend] = useState(false);
+  const [socialState, setsocialState] = useState([false,false,false,false])
 
   useEffect(() => {
     setunkid(DeviceInfo.getUniqueId());
@@ -158,10 +160,12 @@ export default function Verify({navigation}) {
     .then(response => {
       if (response.data == "Limit exceeded") {
           Alert.alert("OTP limit exceeded", "We have send you otp 5 times, check your spam folder or email us with your registerd email address for verification")
-      }
+          setdisableResend(true)
+        }
+
       else if (response.data == "mail sent")
       {
-        Alert.alert("OTP sent again.", "Please check your spam folder, wait 10 min before resending otp, you only have 5 chance total to resend OTP")
+        Alert.alert("OTP sent again.", "Please check your spam folder also, wait 10 min before resending otp, you only have 5 chance total to resend OTP")
       }
     }).catch(error => {
       Alert.alert("Server down" , "Unable to resend otp.")
@@ -182,45 +186,57 @@ export default function Verify({navigation}) {
       <Card style={{margin: 10}}>
         <Card.Content>
           <Button
+            disabled={socialState[0]}
             icon="twitter"
             mode="contained"
             style={styles.mediaBtn}
             onPress={() => {
               Linking.openURL(tweet);
               settwitter(1);
+              var tempar = [true,socialState[1],socialState[2],socialState[3]]
+              setsocialState(tempar)
             }}>
             Follow on Twitter
           </Button>
 
           <Button
+            disabled={socialState[1]}
             icon="telegram"
             mode="contained"
             style={[styles.mediaBtn, {backgroundColor: '#0396de'}]}
             onPress={() => {
               Linking.openURL(tele);
               setteleg(1);
+              var tempar = [socialState[0],true,socialState[2],socialState[3]]
+              setsocialState(tempar)
             }}>
             Join Telegram Channel
           </Button>
 
           <Button
+            disabled={socialState[2]}
             icon="instagram"
             mode="contained"
             style={[styles.mediaBtn, {backgroundColor: '#e8426c'}]}
             onPress={() => {
               Linking.openURL(insta);
               setinstag(1);
+              var tempar = [socialState[0],socialState[1],true,socialState[3]]
+              setsocialState(tempar)
             }}>
             Follow on Intagram
           </Button>
 
           <Button
+            disabled={socialState[3]}
             icon="youtube"
             mode="contained"
             style={[styles.mediaBtn, {backgroundColor: '#c31e1d'}]}
             onPress={() => {
               Linking.openURL(youtube);
               setytu(1);
+              var tempar = [socialState[0],socialState[1],socialState[2],true];
+              setsocialState(tempar)
             }}>
             Subscribe to Channel
           </Button>
@@ -246,7 +262,9 @@ export default function Verify({navigation}) {
           <Button
             mode="text"
             style={{marginTop: 10}}
-            onPress={() => resendOtp()}>
+            onPress={() => resendOtp()}
+            disabled={disableResend}
+            >
             Resend OTP
           </Button>
         </Card.Content>
